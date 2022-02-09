b92cf2aea45c290fbc1b8aa052215874eb49bcef
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-02-09 17:45:23 +0900

    add

--- Components/Footer.js
+++ Components/Footer.js
@@ -83,9 +83,6 @@ const Footer = () => {
             <a className="text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -95,9 +92,6 @@ const Footer = () => {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -107,10 +101,6 @@ const Footer = () => {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
@@ -121,18 +111,12 @@ const Footer = () => {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path
-                  stroke="none"
                   d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
                 ></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
               </svg>
             </a>
           </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
